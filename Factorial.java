aed47cba20c853ca94c16fee37b7b924855cd5cb
ElangovanKandhasamy <69085563+ElangovanKandhasamy@users.noreply.github.com>
2020-10-11 14:51:41 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -4,8 +4,8 @@ public class Factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 5;
-		long factNumber = 100000L;
+		int i ;
+		long factNumber = 5L;
 		long fact = 1L;
 		for (i = 1; i <= factNumber; i++) {
 			fact = fact * i;
